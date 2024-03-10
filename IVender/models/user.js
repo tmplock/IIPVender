bc11f6a9fea1ade9589ad16963436c6dde7a4edb
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-03-11 02:44:57 +0900

    123

--- IVender/models/user.js
+++ IVender/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
         },
         strID: {
             type: DataTypes.STRING,
-            //unique: true,
+            unique: true,
             allowNull: false,
         },
         strPassword: {
