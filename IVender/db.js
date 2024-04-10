d4b93fd1ba06f5229f6cbcc75b26c2444f9b1d0c
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-04-10 20:10:16 +0900

    Update db.js

--- IVender/db.js
+++ IVender/db.js
@@ -1,26 +1,15 @@
 const Sequelize = require('sequelize');
 
-// //  ORIGINAL
-// const sequelize = new Sequelize({
-//     host: 'db-mysql-sgp1-27012-do-user-11246819-0.b.db.ondigitalocean.com',
-//     database: 'iip',
-//     username: 'iiplive',
-//     password: 'oLOHJkiQACPGuAgj',
-//     dialect: 'mysql',
-//     port:25060,
-//     timezone:'Asia/Seoul'
-// });
-
-//  DEV
 const sequelize = new Sequelize({
-    host: 'db-mysql-sgp1-62759-do-user-11246819-0.c.db.ondigitalocean.com',
+    host: 'db-mysql-sgp1-27012-do-user-11246819-0.b.db.ondigitalocean.com',
     database: 'iip',
-    username: 'doadmin',
-    password: 'AVNS_FYsbSxfV0ADzDF4IiRJ',
+    username: 'iiplive',
+    password: 'oLOHJkiQACPGuAgj',
     dialect: 'mysql',
     port:25060,
     timezone:'Asia/Seoul'
 });
+
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
